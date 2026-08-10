8cd3c150c4ca64751ea2dfae7b47b9036c58a930
ishtiauq <ishtiauq@gmail.com>
2026-08-10 13:40:27 +0600

    new

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.luae3fp6lm8"
+    "revision": "0.h0eu8suop88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
